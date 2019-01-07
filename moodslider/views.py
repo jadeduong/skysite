a01b8f5e2fa2845832e8fbedbd9b3753a6812780
Jade <jade-duong@hotmail.com>
2019-01-07 03:59:38 +0000

    upload page

--- moodslider/views.py
+++ moodslider/views.py
@@ -7,4 +7,8 @@ def index(request):
     return render(request, 'moodslider/index.html')
 
 def upload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
     return render(request, 'moodslider/upload.html')
